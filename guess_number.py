998caba69a79a00d34a644f4395a2e052681c999
Amanda Ngo <amandango273@gmail.com>
2024-02-27 21:58:18 -0600

    Added a limit to the number of guesses and tracks how many attempts are left before revealing answer

--- guess_number.py
+++ guess_number.py
@@ -1,12 +1,16 @@
 number = 10
+i = 5
 
 print("I'm thinking of a number...")
 guess = int(input("What number am I thinking of? "))
 
-while guess != 'q':
+while guess != 'q' and i > 1:
     if int(guess) == number:
         print("Congratulations! You guessed the right number.")
         break
     else:
-        guess = input("Sorry, try again! Or enter q to quit. ")
-
+        i -= 1
+        guess = input(f"Sorry, try again! Or enter q to quit. You have {i} attempts remaining. ")
+        if i == 1:
+            print(f"You are out of attempts! The number was {number}")
+        
